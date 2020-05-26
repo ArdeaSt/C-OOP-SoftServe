e88328efb36249e725d3f2f4e301b677a2166ed8
Vladymyr <58039003+ArdeaSt@users.noreply.github.com>
2020-05-26 17:55:51 +0300

    Add files via upload

---
+++ HomeWork5_Part1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    interface IDeveloper
+    {
+        String Tool { get; set; }
+
+        void Create();
+        void Destroy();
+    }
+
+    class Programmer : IDeveloper, IComparable
+    {
+        private string language;
+
+        public string Tool
+        {
+            get { return language; }
+            set { value = language; }
+        }
+
+        public Programmer()
+        {
+        }
+
+        public Programmer(string tool)
+        {
+            language = tool;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine($"App was created by {Tool} programmer.");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"App was deleted from computer by {Tool} programmer!");
+        }
+
+        public int CompareTo(object obj)
+        {
+            IDeveloper temp = obj as IDeveloper;
+            if (temp != null)
+            {
+                return this.Tool.CompareTo(temp.Tool);
+            }
+            else
+            {
+                throw new ArgumentException("Parameter is not Programmer");
+            }
+        }
+    }
+
+    class Builder : IDeveloper, IComparable
+    {
+        private string tool;
+
+        public Builder()
+        {
+        }
+
+        public Builder(string material)
+        {
+            tool = material;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine($"House was built from {Tool} by builder!");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"House from {Tool} destroyed  by builder!");
+        }
+
+        public string Tool
+        {
+            get { return tool; }
+            set { value = tool; }
+        }
+
+        public int CompareTo(object obj)
+        {
+            IDeveloper temp = obj as IDeveloper;
+            if (temp != null)
+            {
+                return this.Tool.CompareTo(temp.Tool);
+            }
+            else
+            {
+                throw new ArgumentException("Parameter is not Builder");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDeveloper[] developers = new IDeveloper[4];
+            developers[0] = new Builder("Panel");
+            developers[1] = new Programmer("C#");
+            developers[2] = new Programmer("Java");
+            developers[3] = new Builder("Brick");
+            for (int i = 0; i < developers.Length; i++)
+            {
+                developers[i].Create();
+            }
+
+            for (int i = 0; i < developers.Length; i++)
+            {
+                developers[i].Destroy();
+            }
+
+
+            Console.WriteLine($"App built on the language:  {developers[2].Tool}");
+
+            Console.WriteLine($"House was built from:  {developers[3].Tool}");
+
+            for (int i = 0; i < developers.Length; i++)
+            {
+                Console.WriteLine(developers[i].Tool);
+            }
+
+            Array.Sort(developers);
+            Console.WriteLine("******Sorted array **********");
+            for (int i = 0; i < developers.Length; i++)
+            {
+                Console.WriteLine(developers[i].Tool);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ HomeWork5_Part2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<uint, string> dreamteam = new Dictionary<uint, string>();
+            dreamteam.Add(10, "Messi");
+            dreamteam.Add(7, "Ronaldo");
+            dreamteam.Add(1, "Buffon");
+            dreamteam.Add(2, "Maldini");
+            dreamteam.Add(3, "Ramos");
+            dreamteam.Add(4, "Carlos");
+            dreamteam.Add(5, "Drogba");
+
+            Console.WriteLine("Input number of football player in DreamTeam: ");
+            uint number = UInt32.Parse(Console.ReadLine());
+            if (dreamteam.ContainsKey(number))
+            {
+                Console.WriteLine($"This player is {dreamteam[number]}");
+            }
+
+
+            else
+            {
+                Console.WriteLine($"Sorry, but player with number {number} is empty! Try again. ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
