a4c5aa7dbe3320354c3d1dee1c4701e12092ab0e
Vladymyr <58039003+ArdeaSt@users.noreply.github.com>
2020-06-03 19:00:25 +0300

    Add files via upload

---
+++ TaskExtansionMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExtantionMethod
+{
+    public static class UserService
+    {
+        public static string Rational(this int a,int x)
+        {
+            decimal temp =(decimal)a / x;
+
+            return temp.ToString("g2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 1;
+            Console.WriteLine(a.Rational(7));
+            Console.WriteLine(b.Rational(6));
+            Console.ReadLine();
+        }
+    }
+}
