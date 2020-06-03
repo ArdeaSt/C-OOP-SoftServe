292151509a5fe408ee5eb837a55674e844a908b1
Vladymyr <58039003+ArdeaSt@users.noreply.github.com>
2020-06-03 21:02:37 +0300

    Add files via upload

---
+++ TaskExepcion_Part1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskException
+{
+    class Program
+    {
+        public static int Div(int a, int b)
+        {
+            return  a / b;
+        }
+        static void Main(string[] args)
+        {
+          
+
+            try
+            {
+                Console.WriteLine("Input 2 integer number:");
+                Console.WriteLine("Input a=");
+                int a = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("input b=");
+                int b = Int32.Parse(Console.ReadLine());
+                
+
+                if (a <b)
+                {
+                    throw new ArgumentException("Error! The first integer must be biger than second integer!");
+                }
+
+                int ans = Div(a, b);
+                Console.WriteLine($"a/b ={ans}");
+
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("You entered wrong format! " + e.Message);
+              
+
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("You entered big number!" + e.Message);
+            }
+            catch (DivideByZeroException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TaskExepcion_Part2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExepcion_PART2
+{
+    [Serializable]
+    class Program
+    {
+        public static void ReadNumber(int start, int end)
+        {
+            Console.WriteLine("Input 10 numbers");
+            int[] arr = new int[10];
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine($"Digit numer {i + 1}  :");
+                    arr[i] = Int32.Parse(Console.ReadLine());
+                }
+
+                Array.Sort(arr);
+
+                if (arr[0] > start & arr[9] < end)
+                {
+                    Console.WriteLine($"The integer numbers are in the range: {start}...{end} ");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+                else
+                {
+                    throw new Exception($"The integer numbers are out of the range {start}...{end}!!!");
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ReadNumber(1, 100);
+            Console.ReadLine();
+        }
+    }
+}
